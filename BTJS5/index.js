da7a7f81629ee97e23fc5386aafd9843204de583
hothanhminhhuong <97611274+hothanhminhhuong@users.noreply.github.com>
2022-03-02 14:12:15 +0700

    Add files via upload

--- BTJS5/index.js
+++ BTJS5/index.js
@@ -1,4 +1,4 @@
-// bài 1
+// BÀI 1
 const score_area_A = 2;
 const score_area_B = 1;
 const score_area_C = 0.5;
@@ -18,85 +18,232 @@ document.getElementById("score_calc").onclick = function () {
     const pass = document.getElementById("score_pass");
     const actual = document.getElementById("score_actual");
     let score = 0;
-    if (sjA + sjB + sjC < boardScore) {
-        pass.innerHTML = `Rớt`;
-        actual.innerHTML = `${sum1}`;
-    }
+    // if (sum1 + score_area_A + score_target_1 < boardScore) {
+    //     pass.innerHTML = `Rớt`;
+    //     actual.innerHTML = `${sum1 + score_area_A + score_target_1 }`;
+    // } else if {
+
+    // }
     if (sjA == 0 || sjB == 0 || sjC == 0) {
         pass.innerHTML = `Rớt`;
-        actual.innerHTML = `${sum1}`;
+        if (area === "A") {
+            if (target == 0) {
+            score = sum1 + score_area_A;
+            actual.innerHTML = `${score}`;
+            } else if (target == 1) {
+                score = sum1 + score_area_A + score_target_1;
+                actual.innerHTML = `${score}`;
+            } else if (target == 2) {
+                score = sum1 + score_area_A + score_target_2;
+                actual.innerHTML = `${score}`;
+            } else if (target == 3) {
+                score = sum1 + score_area_A + score_target_3;
+                actual.innerHTML = `${score}`;
+            }
+        } else  if (area === "B") {
+        if (target == 0) {
+        score = sum1 + score_area_B;
+        actual.innerHTML = `${score}`;
+        } else if (target == 1) {
+            score = sum1 + score_area_B + score_target_1;
+            actual.innerHTML = `${score}`;
+        } else if (target == 2) {
+            score = sum1 + score_area_B + score_target_2;
+            actual.innerHTML = `${score}`;
+        } else if (target == 3) {
+            score = sum1 + score_area_B + score_target_3;
+            actual.innerHTML = `${score}`;
+        }
+        } else if (area === "C") {
+            if (target == 0) {
+            score = sum1 + score_area_C;
+            actual.innerHTML = `${score}`;
+            } else if (target == 1) {
+                score = sum1 + score_area_C + score_target_1;
+                actual.innerHTML = `${score}`;
+            } else if (target == 2) {
+                score = sum1 + score_area_C + score_target_2;
+                actual.innerHTML = `${score}`;
+            } else if (target == 3) {
+                score = sum1 + score_area_C + score_target_3;
+                actual.innerHTML = `${score}`;
+        }
+    
+        } else if (area === "X") {
+        if (target == 0) {
+        score = sum1;
+        actual.innerHTML = `${score}`;
+        } else if (target == 1) {
+            score = sum1 + score_target_1;
+            actual.innerHTML = `${score}`;
+        } else if (target == 2) {
+            score = sum1 + score_target_2;
+            actual.innerHTML = `${score}`;
+        } else if (target == 3) {
+            score = sum1 + score_target_3;
+            actual.innerHTML = `${score}`;
+        }
     }
-    if (sjA + sjB + sjC >= boardScore && sjA > 0 && sjB > 0 && sjC > 0 ){
-        if (area === "x" || area === "X"){
-            if (target == 1){
-                score += sum1 + score_target_1;
-                pass.innerHTML = `Đậu`;
+    } else if (sjA > 0 && sjB > 0 && sjC > 0) {
+        if (area === "A") {
+            if (target == 0) {
+                score = sum1 + score_area_A;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            } else if (target == 1) {
+                score = sum1 + score_area_A + score_target_1;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            } else if (target == 2) {
+                score = sum1 + score_area_A + score_target_2;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            } else if (target == 3) {
+                score = sum1 + score_area_A + score_target_3;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            }
+        } else if (area === "B") {
+            if (target == 0) {
+                score = sum1 + score_area_B;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            } else if (target == 1) {
+                score = sum1 + score_area_B + score_target_1;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`; 
+                    actual.innerHTML = `${score}`;
+                }
+            } else if (target == 2) {
+                score = sum1 + score_area_B + score_target_2;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {    
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            } else if (target == 3) {
+                score = sum1 + score_area_B + score_target_3;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            }
+        } else if (area === "C") {
+            if (target == 0) {
+                score = sum1 + score_area_C;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                    }
+            } else if (target == 1) {
+                score = sum1 + score_area_C + score_target_1;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            } else if (target == 2) {
+                score = sum1 + score_area_C + score_target_2;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            } else if (target == 3) {
+                score = sum1 + score_area_C + score_target_3;
+                if (score >= boardScore) {
+                    pass.innerHTML = `đậu`;
+                    actual.innerHTML = `${score}`;
+                } else {
+                    pass.innerHTML = `Rớt`;
+                    actual.innerHTML = `${score}`;
+                }
+            }   
+        }else if (area === "X") {
+        if (target == 0) {
+            score = sum1;
+            if (score >= boardScore) {
+                pass.innerHTML = `đậu`;
+                actual.innerHTML = `${score}`;
+            } else {
+                pass.innerHTML = `Rớt`;
                 actual.innerHTML = `${score}`;
             }
-            if (target == 2){
-                score += sum1 + score_target_2;
-                pass.innerHTML = `Đậu`;
+        } else if (target == 1) {
+            score = sum1 + score_target_1;
+            if (score >= boardScore) {
+                pass.innerHTML = `đậu`;
+                actual.innerHTML = `${score}`;
+            } else {
+                pass.innerHTML = `Rớt`;
                 actual.innerHTML = `${score}`;
             }
-            if (target == 3){
-                score += sum1 + score_target_3;
-                pass.innerHTML = `Đậu`;
+        } else if (target == 2) {
+            score = sum1 + score_target_2;
+            if (score >= boardScore) {
+                pass.innerHTML = `đậu`;
+                actual.innerHTML = `${score}`;
+            } else {
+                pass.innerHTML = `Rớt`;
                 actual.innerHTML = `${score}`;
             }
-        } else if (area === "a" || area === "A"){
-            if (target == 1){
-                score += sum1 + score_target_1 + score_area_A;
-                pass.innerHTML = `Đậu`;
+        } else if (target == 3) {
+            score = sum1 + score_target_3;
+            if (score >= boardScore) {
+                pass.innerHTML = `đậu`;
+                actual.innerHTML = `${score}`;
+            } else {
+                pass.innerHTML = `Rớt`;
                 actual.innerHTML = `${score}`;
             }
-            if (target == 2){
-                score += sum1 + score_target_2 + + score_area_A;
-                pass.innerHTML = `Đậu`;
-                actual.innerHTML = `${score}`;
-            }
-            if (target == 3){
-                score += sum1 + score_target_3 + + score_area_A;
-                pass.innerHTML = `Đậu`;
-                actual.innerHTML = `${score}`;
-            }
-        } else if (area === "b" || area === "B"){
-            if (target == 1){
-                score += sum1 + score_target_1 + score_area_B;
-                pass.innerHTML = `Đậu`;
-                actual.innerHTML = `${score}`;
-            }
-            if (target == 2){
-                score += sum1 + score_target_2 + + score_area_B;
-                pass.innerHTML = `Đậu`;
-                actual.innerHTML = `${score}`;
-            }
-            if (target == 3){
-                score += sum1 + score_target_3 + + score_area_B;
-                pass.innerHTML = `Đậu`;
-                actual.innerHTML = `${score}`;
-            }
-        } else if (area === "c" || area === "C"){
-            if (target == 1){
-                score += sum1 + score_target_1 + score_area_C;
-                pass.innerHTML = `Đậu`;
-                actual.innerHTML = `${score}`;
-            }
-            if (target == 2){
-                score += sum1 + score_target_2 + + score_area_C;
-                pass.innerHTML = `Đậu`;
-                actual.innerHTML = `${score}`;
-            }
-            if (target == 3){
-                score += sum1 + score_target_3 + + score_area_C;
-                pass.innerHTML = `Đậu`;
-                actual.innerHTML = `${score}`;
-            }
+        }
         }
     }
 }
 
 
-// bài 2
+
+// BÀI 2
 const KW_1_50 = 500;
 const KW_51_100 = 650;
 const KW_101_200 = 850;
@@ -113,31 +260,32 @@ document.getElementById("pay").onclick = function () {
         price1 += kw * KW_1_50;
     }
     else if (kw > 50 && kw <= 100) {
-        price1 += 50 * KW_1_50;
-        price2 += (kw - 50) * KW_51_100;
+        price1 = 50 * KW_1_50;
+        price2 = (kw - 50) * KW_51_100;
     }
     else if (kw > 100 && kw <= 200) {
-        price1 += 50 * KW_1_50;
-        price2 += 50 * KW_51_100;
-        price3 += (kw - 100) * KW_101_200;
+        price1 = 50 * KW_1_50;
+        price2 = 50 * KW_51_100;
+        price3 = (kw - 100) * KW_101_200;
     }
     else if (kw > 200 && kw <= 350) {
-        price1 += 50 * KW_1_50;
-        price2 += 50 * KW_51_100;
-        price3 += 100 * KW_101_200;
-        price4 += (kw - 200) * KW_201_350;
+        price1 = 50 * KW_1_50;
+        price2 = 50 * KW_51_100;
+        price3 = 100 * KW_101_200;
+        price4 = (kw - 200) * KW_201_350;
     }
     else if (kw > 350) {
-        price1 += 50 * KW_1_50;
-        price2 += 50 * KW_51_100;
-        price3 += 100 * KW_101_200;
-        price4 += 350 * KW_201_350;
-        price5 += (kw - 350) * KW_351;
+        price1 = 50 * KW_1_50;
+        price2 = 50 * KW_51_100;
+        price3 = 100 * KW_101_200;
+        price4 = 350 * KW_201_350;
+        price5 = (kw - 350) * KW_351;
     }
     const wrapResultEl = document.getElementById("result");
     const total = price1 + price2 + price3 + price4 + price5;
     wrapResultEl.style.display = "block";
 
+    
     document.getElementById("price_total").innerHTML = total.toLocaleString("vn-VI", { style: 'currency', currency: 'VND' });
 
 }
